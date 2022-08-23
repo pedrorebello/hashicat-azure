4a6fb8209ee32074d4e8bf18828382ae6c4cc8d1
pedrorebello <65690682+pedrorebello@users.noreply.github.com>
2022-08-23 16:23:40 -0400

    Update network.tf

--- network.tf
+++ network.tf
@@ -2,4 +2,9 @@ module "network" {
   source  = "app.terraform.io/kyn-tf-workshop/network/azurerm"
   version = "3.5.0"
   resource_group_name = azurerm_resource_group.myresourcegroup.name
+  
+  tags = {
+    Billable = "boss"
+    Department = "CIO"
+  }
 }
